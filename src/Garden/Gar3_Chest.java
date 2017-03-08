9d891fbfa0ee4bf0d2c110e7e8621ce88ffd0bda
KevinRapa <kevjrapa@gmail.com>
2017-03-08 12:05:54 -0500

    Update Gar3_Chest.java

--- src/Garden/Gar3_Chest.java
+++ src/Garden/Gar3_Chest.java
@@ -3,16 +3,18 @@ package Garden;
 import A_Super.Item;
 import A_Super.Openable;
 import A_Super.SearchableFurniture;
+import static A_Main.Id.GCHS;
 /**
  * @author Kevin Rapa
  */
-public class Gar3_Chest extends SearchableFurniture implements Openable {
+public class Gar3_Chest extends LockedCabinet {
     // ========================================================================
     public Gar3_Chest (Item... items) {
-        super(items);
+        super(GCHS, items);
         
+        this.actDialog = "It takes a small bit of force, but the rusty key manages to open the chest.";
         this.description = "It's a wooden chest for the holding of gardening implements and other nonsense.";
-        this.searchDialog = "You open the chest and peer inside.";
+        this.searchDialog = "To your dismay, the chest has been locked previously.";
 
         this.addNameKeys("(?:wooden )?(?:utility )?chest");
     }
