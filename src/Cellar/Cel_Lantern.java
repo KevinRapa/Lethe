5fcf328a377a1f4b94df80809dd173da9d4685a5
KevinRapa <kevjrapa@gmail.com>
2017-04-29 16:54:48 -0400

    Update Cel_Lantern.java

--- src/Cellar/Cel_Lantern.java
+++ src/Cellar/Cel_Lantern.java
@@ -10,8 +10,8 @@ public class Cel_Lantern extends Furniture {
     public Cel_Lantern () {
         super();
         
-        this.description = "The small octogonal gas lamp hangs from a chain "
-                + "connected to the ceiling. It lights the area is a dim orange "
+        this.description = "The small octagonal gas lamp hangs from a chain "
+                + "connected to the ceiling. It lights the area in a dim orange "
                 + "hue.";
         this.actDialog = "The lantern is connected to a chain on the ceiling and "
                 + "can't be moved.";
