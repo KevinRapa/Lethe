698b1e961d4f37023e80e0482292e15c89cf120a
KevinRapa <kevjrapa@gmail.com>
2017-04-29 17:15:35 -0400

    Update Lib2_Window.java

--- src/Library/Lib2_Window.java
+++ src/Library/Lib2_Window.java
@@ -10,10 +10,7 @@ public class Lib2_Window extends Window {
         this.descClosed = "Through the small stone window, you can see your "
                         + "village in the great distance to the east. Ahead "
                         + "is just an expanse of sea and fog.";
-        this.descOpen = "Through the small stone window, you can see your "
-                      + "village in the great distance to the east. Ahead "
-                      + "is just an expanse of sea and fog. A light breeze "
-                      + "flows in.";
+        this.descOpen = this.descClosed + " A light breeze flows in.";
     }
 //-----------------------------------------------------------------------------    
 }
