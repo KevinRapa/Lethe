62cec5cbb119ee363b844522745da663a7f4563f
KevinRapa <kevjrapa@gmail.com>
2017-04-29 15:41:15 -0400

    Update Att_Cases.java

--- src/Attic/Att_Cases.java
+++ src/Attic/Att_Cases.java
@@ -14,7 +14,7 @@ public class Att_Cases extends SearchableFurniture implements Openable, Moveable
         
         this.description = "Various suitcases of different sizes lie stacked up around the attic. They seem to be filled with just clothes.";
         this.actDialog = "How greedy you are. Surely you couldn't carry all of these suitcases around.";
-        this.searchDialog = "There are plenty of suitcases. You open a few of them at random.";
+        this.searchDialog = "You pick through the various suitcases scattered about.";
 
         this.addNameKeys("(?:suit)?cases?", "piles");
         this.addActKeys(GETPATTERN);
